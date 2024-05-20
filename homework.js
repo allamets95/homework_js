1e6e421f36acd1141ac7ca789d17444fa76f7a80
Alla Mets <allamets1607@gmail.com>
2024-05-19 21:08:02 -0400

    task 2 implemented

--- homework.js
+++ homework.js
@@ -6,3 +6,13 @@ var task1 = 'number' + 3 + 3
 // 'number3' + 3 -> 'number33' (перетворення числа 3 на рядок і конкатенація)
 // Result: 'number33'
 console.log("Task 1: " + task1)
+
+
+// Task 2: null + 3
+
+var task2 = null + 3
+// Answer:
+// null перетворюється на 0
+// 0 + 3 -> 3
+// Result: 3
+console.log("Task 2: " + task2)
